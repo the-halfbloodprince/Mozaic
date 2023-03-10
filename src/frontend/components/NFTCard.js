7ef131ea367ecf8bf186b0d2d58478ef6d7cf465
Manish Kumar Das <ammanishkumardas@gmail.com>
2023-03-11 01:12:42 +0530

    tilt effects

--- src/frontend/components/NFTCard.js
+++ src/frontend/components/NFTCard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './NFTCard.module.css'
 import { ethers } from "ethers";
 import { useNavigate } from 'react-router';
+import Tilt from 'react-parallax-tilt';
 
 const NFTCard = ({ nft: {itemId, name, image, totalPrice, onSale, category}, actionText = null, actionFunc = null }) => {
 // console.log(price);
@@ -9,17 +10,20 @@ const NFTCard = ({ nft: {itemId, name, image, totalPrice, onSale, category}, act
     const navigate = useNavigate()
 
     return (
-    <div className={styles.nftcard}>
-        { actionText && <div className={styles.action} onClick={actionFunc} > {actionText} </div> }
-        <div onClick={() => navigate(`/nft/${itemId}`)}>
-            <img className={styles.img} src={image} alt={name} />
-            <p className={styles.category}> {category} </p>
-            <div className={styles.info}>
-                <p className={styles.name}> {name} </p>
-                <p className={styles.price}> {onSale ? `${ethers.utils.formatEther(totalPrice)} ETH` : 'Unlisted'} </p>
+        <Tilt glareEnable={true} glareMaxOpacity={.7} tiltMaxAngleX={10} tiltMaxAngleY={10}>
+            <div className={styles.nftcard}>
+                { actionText && <div className={styles.action} onClick={actionFunc} > {actionText} </div> }
+                <div onClick={() => navigate(`/nft/${itemId}`)}>
+                    <img className={styles.img} src={image} alt={name} />
+                    <p className={styles.category}> {category} </p>
+                    <div className={styles.info}>
+                        <p className={styles.name}> {name} </p>
+                        <p className={styles.price}> {onSale ? `${ethers.utils.formatEther(totalPrice)} ETH` : 'Unlisted'} </p>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>)
+        </Tilt>    
+    )
 }
 
 
